4cd97f2d84bfb482d4e41bb483b13db93a890248
AlexandraZbant <alexandrazbant@gmail.com>
2023-01-05 16:49:31 +0200

    initial commit

---
+++ src/test/java/com/alexandrazbant/SpringDataJPAJDBC/SpringDataJpaJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandrazbant.SpringDataJPAJDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
