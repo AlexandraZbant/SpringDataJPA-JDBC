93da91de9fa5e24778e1dee40b0fe19244de628f
AlexandraZbant <alexandrazbant@gmail.com>
2023-01-07 20:36:48 +0200

    adding config and initial data

---
+++ src/main/java/com/alexandrazbant/SpringDataJPAJDBC/domain/Author.java
@@ -0,0 +1,20 @@
+package com.alexandrazbant.SpringDataJPAJDBC.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+}
---
+++ src/main/java/com/alexandrazbant/SpringDataJPAJDBC/domain/Book.java
@@ -0,0 +1,34 @@
+package com.alexandrazbant.SpringDataJPAJDBC.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String isbn;
+    private String publisher;
+    private Long authorId;
+
+
+    public Book(String title, String isbn, String publisher, Long authorId) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.authorId = authorId;
+    }
+}
+
+
