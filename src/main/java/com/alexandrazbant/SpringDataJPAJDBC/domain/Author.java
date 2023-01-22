82b18d55c42bc7c6fbc3647973945cf1a3cac5f1
AlexandraZbant <alexandrazbant@gmail.com>
2023-01-22 21:57:23 +0200

    add books in authorDao

--- src/main/java/com/alexandrazbant/SpringDataJPAJDBC/domain/Author.java
+++ src/main/java/com/alexandrazbant/SpringDataJPAJDBC/domain/Author.java
@@ -1,14 +1,20 @@
 package com.alexandrazbant.SpringDataJPAJDBC.domain;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
-@EqualsAndHashCode
 public class Author {
 
     @Id
@@ -17,4 +23,20 @@ public class Author {
 
     private String firstName;
     private String lastName;
+
+    @Transient
+    private List<Book> books;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Author author = (Author) o;
+        return id != null && Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
